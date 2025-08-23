20f027dfaf1f9d51d2df615e036ec891b0d5c980
yewon1020 <yewon131020@gmail.com>
2025-08-23 17:41:48 +0900

    create shop

---
+++ app.py
@@ -0,0 +1,42 @@
+import requests
+import requests
+
+from flask import Flask, render_template, jsonify,request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/menu")
+def get_menu() :
+    return jsonify({"menu" : "팥 붕어빵, 슈크림 붕어빵, 고구마 붕어빵, 치즈 붕어빵, 피자 호빵"})
+
+@app.route("/sales")
+def get_sales() :
+    return jsonify({"sales" : "200000원"})
+
+@app.route("/coupon")
+def get_coupon() :
+    return jsonify({"coupon" : "붕어빵 1개 무료 쿠폰!, 10% 할인 쿠폰"})
+
+@app.route("/order",methods = ["POST"])
+def get_order() :
+    data = request.get_json()
+    print(data)
+    name = data.get("name")
+    coupon = data.get("coupon")
+    type_b = data.get("type")
+    type_name = data.get("type_name")
+
+    amount = int(data.get("amount"))
+    coupon = int(coupon)
+    price = int(type_b)
+    price = (1 - coupon/100) * price * amount
+
+    return jsonify({"msg" : f"{name} 손님, {type_name} 주문이 완료되었습니다. 금액은 {price}원입니다."})
+
+if __name__ == '__main__':
+# debug=True 모드는 개발 중에만 사용해야 합니다.
+    app.run(debug=True, port=5002)
